842a7d0d0ab8bcb11232b1f13b99f6742abb5c13
Kunal Kharalkar <kunalkharalkar@gmail.com>
2023-03-17 16:30:00 +0530

    Database Connections Modified

---
+++ src/configs/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    dev : 'mongodb://localhost:27017/99acres-app',
+    prod : 'mongodb+srv://kunalkharalkar:Panda1999@99acres-app.e4fj81n.mongodb.net/99acres-app'
+}
--- src/utils/db.connection.js
+++ src/utils/db.connection.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose'); //import mongoose
 
+const dbConfig = require('../configs/db.config.js'); //import dbConfig
+
 
 const connect = () => { //connecting database
     console.log('Initializing Database Connection');
-    mongoose.connect('mongodb+srv://kunalkharalkar:Panda1999@99acres-app.e4fj81n.mongodb.net/99acres-app')
+    mongoose.connect(dbConfig.prod)
     .then(()=>{
         console.log('Database connected successfully.');
     })
